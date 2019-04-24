f4b1f204fc87cdc2d77835278b5cad010c600143
Jan Jakubowski <jan.jakubowski.us@ieee.org>
2019-04-24 18:58:09 -0400

    no css git add -A! intro page, start button and timeout working, create flash card object with answers scrambled working to console.log

--- assets/js/app.1.js
+++ assets/js/app.1.js
@@ -1,9 +1,9 @@
 // app.js -- Stargate SG-1 Trivia Game -- Jan Jakubowski
 var tempQ = " ";
 var tempA = [ ];
-// var answer = { txt : " ", correct : false};
+var answer = { txt : " ", correct : false};
 var randomOrder = [0, 1, 2, 3];
-// var currentQ = " ";
+var currentQ = " ";
 const triviaBank = {
     round : 1,
     questions : [ 
@@ -17,6 +17,8 @@ const triviaBank = {
         }
     ]
      }
+    //  create : function(answers) 
+// };
 
 function triviaCard(Q,A,index) {
     this.Q = Q,
@@ -25,13 +27,15 @@ function triviaCard(Q,A,index) {
 }
 
 shuffle = function (arrayIn) {
-    randomize();
+    // this.Q = tempQ;
     var arrayOut = [];
+    console.log(arrayIn);
     for (var x=0; x < arrayIn.length; x++ ) { 
         var random =  randomOrder[x];
         console.log("random element:" + random + " | arrayIn:" + arrayIn[random]);
         arrayOut[x] = arrayIn[random];
-    }
+     }
+
     console.log(arrayOut);
     return arrayOut;
 }
@@ -39,8 +43,8 @@ shuffle = function (arrayIn) {
 var currentGame = {
     round : 0
 }
-
 function randomize() {
+    // debugger;
     var tempOrder = randomOrder;
     randomOrder = [];
     for (var i=0; i<4; i++) {
@@ -52,12 +56,17 @@ function randomize() {
 
 function clearAll () {$("#intro").empty();}
 
+// for (var i=1; i<=10; i++) {
+//     randomize ();
+// }
+
 
 
 var welcomePara = $("<p>").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
 var startGameBtn = $("<button>");
 startGameBtn.addClass("startGame");
 startGameBtn.text("Start Game");
+// startGameBtn.addClass(.......);
 
 $("#intro").append(welcomePara);
 $("#intro").append(startGameBtn);
@@ -74,16 +83,27 @@ function makeCard () {
         var tempQ = triviaBank.questions[i].question;
         var tempA = [triviaBank.questions[i].correct];
         var tempA = tempA.concat(triviaBank.questions[i].incorrect);
+       
+        // console.log("i: " + tempIndex);
+        console.log(tempQ + " | " + tempA);
+        randomize();
         tempA = shuffle(tempA);
         for (var j=0; j < 4; j++) {
             if (tempA[j] == triviaBank.questions[i].correct) {
                 var tempIndex = j;
             }
         }
+        console.log ("index: " + tempIndex);
         var currentQnA = new triviaCard(tempQ,tempA,tempIndex);
+        
+        // debugger;
+        
+        // console.log(tempQ + " | " + tempA);
+        
+        // shuffle(currentQnA.A);
         console.log(currentQnA.Q + " | " + currentQnA.A + " | " + currentQnA.correctIndex);
     }
 }
+// });
 
 
-// END OF FILE 
